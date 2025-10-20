1fc53bc9704f552951a018f3f8e594498fd8b3c1
Bartosz <161606293+brts-u@users.noreply.github.com>
2025-10-20 14:56:25 +0200

    dodaje wizualizacje i kilka funkcji do implementacji
    coś dopisałem już sam nw co

--- shp_graph.py
+++ shp_graph.py
@@ -1,52 +1,47 @@
 """
-Moduł z klasą graf oraz funkcjami do tworzenia grafu z pliku shapefile.
+Moduł z klasą grafu sieci drogowej oraz funkcjami do tworzenia go z pliku shapefile SKJZ (jezdni) w strukturze BDOT10k.
 
-Autor: Bartosz Urbanek, 331931
+Autor: Bartosz Urbanek 331931
 Data: 2025-10-17
 """
 from __future__ import annotations
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 import numpy as np
 import geopandas as gpd
+import matplotlib.pyplot as plt
 
-
-shp_file_paths = ["kujawsko_pomorskie_m_Torun/L4_1_BDOT10k__OT_SKJZ_L.shp", "kujawsko_pomorskie_pow_torunski/L4_2_BDOT10k__OT_SKJZ_L.shp"]
-
-test_path = "test_shp.shp"
-
-# gdf = gpd.read_file(shp_file_paths[0])._append(gpd.read_file(shp_file_paths[1]), ignore_index=True)
-# iterator = gdf.iterfeatures(drop_id=True)
-# while True:
-#     try:
-#         feature = next(iterator)
-#     except StopIteration:
-#         break
-#     print(feature["properties"]["idIIP_BT_I"] ,feature["properties"]["klasaDrogi"], feature["geometry"]["coordinates"][0], feature["geometry"]["coordinates"][-1])
 
 def euclidean_distance(coord1: Tuple[float, float], coord2: Tuple[float, float]) -> float:
+    # Odległość między punktami
     return np.sqrt((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2)
 def approx(a: float, b: float, tol: float = 0.10) -> bool:
+    # Przybliżenie
     return abs(a - b) < tol
 def point_approx(coord1: Tuple[float, float], coord2: Tuple[float, float], tol: float = 0.10) -> bool:
+    # Przybliżenie dla punktów (krotek 2D)
     return approx(coord1[0], coord2[0], tol) and approx(coord1[1], coord2[1], tol)
 def node_id(x: float, y: float) -> str:
+    # ID węzła na podstawie współrzędnych (może lepiej robić jakoś inaczej?)
     return f'({x} {y})'
 
+# Klasa węzła grafu
 class Node:
     def __init__(self, x: float, y: float):
-        self.id = node_id(x, y)
-        self.x = x
-        self.y = y
-        self.edges = []
+        self.id: str = node_id(x, y)
+        self.x: float = x
+        self.y: float = y
+        self.edges: List[Edge] = []
 
-    def add_edge(self, edge: 'Edge'):
-        self.edges.append(edge)
+    # Zwraca listę sąsiadów i drogi do nich, sąsiady wielopowiązane rozróżnialne przez ID krawędzi
+    def get_neighbours(self) -> List[Tuple[Node, Edge]]:
+        return [(edge.get_other_node(self), edge) for edge in self.edges]
+        # Teoretycznie powinien być blok Try-Except, ale to by oznaczało, że graf został błędnie zbudowany
 
-    def get_neighbours(self) -> List[Tuple['Node', 'Edge']]:
-        return [(edge.get_other_node(), edge) for edge in self.edges]
-        # Teoretycznie powinien być blok Try-Except, ale to by oznaczało, że graf jest błędnie zbudowany
+    def heuristic(self, other: Node) -> float:
+        return euclidean_distance((self.x, self.y), (other.x, other.y))
 
+# Prędkości przypisane do klas dróg w km/h
 speed = {   # Okazuje się, że nie da się tego zmapować tak 1:1, więc wybrałem takie, które mają największy sens
     "A": 140,   # Autostrada
     "S": 120,   # Droga ekspresowa
@@ -74,24 +69,26 @@ class Edge:
         else:
             raise ValueError("Node not part of this edge")
 
+    # Koszt przejazdu krawędzi w sekundach
     def cost(self) -> float:
-        return self.length * 3600 / speed.get(self.classification)
+        return self.length * 3.6 / speed.get(self.classification)
 
 class Graph:
     def __init__(self):
         self.nodes = {}
         self.edges = {}
+        self.shapefile_path: str | List[str] = ""
 
     def add_node(self, node: Node):
         self.nodes[node.id] = node
 
     def add_edge(self, edge: Edge):
         self.edges[edge.id] = edge
-        edge.start_node.add_edge(edge)
+        edge.start_node.edges.append(edge)
         if not edge.one_way:
-            edge.end_node.add_edge(edge)
+            edge.end_node.edges.append(edge)
 
-    def add_feature(self, feature: dict):
+    def add_feature(self, feature: Dict):
         coords = feature["geometry"]["coordinates"]
         start_coord = coords[0]
         end_coord = coords[-1]
@@ -101,12 +98,12 @@ class Graph:
         if not start_node:
             start_node = Node(start_coord[0], start_coord[1])
             self.add_node(start_node)
-
         end_node = self.nodes.get(node_id(end_coord[0], end_coord[1]))
         if not end_node:
             end_node = Node(end_coord[0], end_coord[1])
             self.add_node(end_node)
 
+        # Długość polilinii w metrach
         # Shapely.length może szybsza metoda? Nie sprawdzałem, ale chyba nie da się tu nic zoptymalizować, może tylko for-loop?
         length = 0.0
         for i in range(len(coords) - 1):
@@ -118,22 +115,59 @@ class Graph:
         edge = Edge(feature["properties"]["idIIP_BT_I"], start_node, end_node, length, classification)
         self.add_edge(edge)
 
-    # TODO: implement
+    # TODO: implement, może robić na poziomie tworzenia grafu?
     def find_nearest_node(self, coordinates: Tuple[float, float]) -> Node:
         pass
 
-def build_graph_from_shapefile(shp_path: str) -> Graph:
-    gdf = gpd.read_file(shp_path)
-    graph = Graph()
+    # TODO: implement
+    def reconstruct_shp_path(self, path: List[Edge]):
+        pass
+
+def draw_graph(G: Graph):
+    # Rysowanie dla dużych grafów jest bardzo, bardzo wolne i pewnie nieczytelne, szczególnie rysowanie węzłów,
+    # można rysować same krawędzie, ale i tak obrazek będzie jedynie do potwierdzenia, że "coś" zrobił
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    for edge in G.edges.values():
+        x_coords = [edge.start_node.x, edge.end_node.x]
+        y_coords = [edge.start_node.y, edge.end_node.y]
+        ax.plot(x_coords, y_coords, color='blue', linewidth=0.5)
+
+    for node in G.nodes.values():
+        ax.scatter(node.x, node.y, color='k', s=5)
+
+    ax.set_title("Wizualizacja grafu")
+    ax.set_xlabel("Easting")
+    ax.set_ylabel("Northing")
+
+def build_graph_from_shapefile(file_path: str | List[str]) -> Graph:
+    if isinstance(file_path, list):
+        gdf = gpd.read_file(file_path[0])
+        for p in file_path[1:]:
+            gdf = gdf._append(gpd.read_file(p), ignore_index=True)
+    else:
+        gdf = gpd.read_file(file_path)
+    G = Graph()
+    G.shapefile_path = file_path
+
+    # Iteracja po obiektach wczytanych do GeoDataFrame
     iterator = gdf.iterfeatures(drop_id=True)
     while True:
         try:
             feature = next(iterator)
         except StopIteration:
             break
-        graph.add_feature(feature)
-    return graph
+        G.add_feature(feature)
+    return G
 
+# Demo
 if __name__ == "__main__":
+    shp_file_paths = ["kujawsko_pomorskie_m_Torun/L4_1_BDOT10k__OT_SKJZ_L.shp",
+                      "kujawsko_pomorskie_pow_torunski/L4_2_BDOT10k__OT_SKJZ_L.shp"]
+    test_path = "test_shp.shp"
+
     graph = build_graph_from_shapefile(test_path)
     print(f"Graph has {len(graph.nodes)} nodes and {len(graph.edges)} edges.")
+
+    draw_graph(graph)
+    plt.show()
