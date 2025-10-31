e012125a2a78df6e40966035588724725264dfe7
brts-u <burb204@gmail.com>
2025-10-31 13:29:05 +0100

    scalanie bliskich końców polilini tym razem działa

--- shp_graph.py
+++ shp_graph.py
@@ -13,23 +13,21 @@ from scipy.spatial import KDTree
 import matplotlib.pyplot as plt
 import random
 
+def floor(a: float, n: int):
+    return np.floor(a * 10**n) / 10**n
+def ceil(a: float, n: int):
+    return np.ceil(a * 10**n) / 10**n
 def euclidean_distance(coord1: Tuple[float, float], coord2: Tuple[float, float]) -> float:
     # Odległość między punktami
     return np.sqrt((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2)
 def approx(a: float, b: float, tol: float = 0.10) -> bool:
     # Przybliżenie
     return abs(a - b) < tol
-def point_approx(coord1: Tuple[float, float], coord2: Tuple[float, float], tol: float = 0.10) -> bool:
-    # Przybliżenie dla punktów (krotek 2D)
-    return approx(coord1[0], coord2[0], tol) and approx(coord1[1], coord2[1], tol)
-def node_id(x: float, y: float) -> str:
-    # ID węzła na podstawie współrzędnych (może lepiej robić jakoś inaczej?)
-    return f'({x} {y})'
 
 # Klasa węzła grafu
 class Node:
     def __init__(self, x: float, y: float):
-        self.id: str = node_id(x, y)
+        self.id: int = -1
         self.x: float = x
         self.y: float = y
         self.edges: List[Edge] = []
@@ -97,19 +95,16 @@ class Graph:
         if not edge.one_way:
             edge.end_node.edges.append(edge)
 
-    def add_feature(self, feature: Dict):
+    def add_feature(self, feature: Dict, manager: NodeManager):
         coords = feature["geometry"]["coordinates"] # z modułu geometry pobiera tylko coordinates
         start_coord = coords[0]
         end_coord = coords[-1]
 
-        # TODO: zakłada, że końce się idealnie pokrywają - powinno wykorzystywać funkcję point_approx z tol = 0.1
-        start_node = self.nodes.get(node_id(start_coord[0], start_coord[1]))
-        if not start_node:
-            start_node = Node(start_coord[0], start_coord[1])
+        start_node = manager.manage_new_node(start_coord[0], start_coord[1])
+        if start_node not in self.nodes:
             self.add_node(start_node)
-        end_node = self.nodes.get(node_id(end_coord[0], end_coord[1]))
-        if not end_node:
-            end_node = Node(end_coord[0], end_coord[1])
+        end_node = manager.manage_new_node(end_coord[0], end_coord[1])
+        if end_node not in self.nodes:
             self.add_node(end_node)
 
         # Długość polilinii w metrach
@@ -118,17 +113,19 @@ class Graph:
         for i in range(len(coords) - 1):
             length += euclidean_distance((coords[i][0], coords[i][1]), (coords[i + 1][0], coords[i + 1][1]))
 
-        classification = feature["properties"]["KLASA_DROG"]
+        classification = feature["properties"]["klasaDrogi"]
         # TODO: obsługa dróg jednokierunkowych (?)
 
-        edge = Edge(feature["properties"]["LOKALNYID"], start_node, end_node, length, classification)
+        edge = Edge(feature["properties"]["idIIP_BT_I"], start_node, end_node, length, classification)
         self.add_edge(edge)
 
     def find_nearest_node(self, coordinates: Tuple[float, float]) -> Node:
         self.build_kdtree()
         _, idx = self._kdtree.query(coordinates)
         nearest_coord = self._node_coords[idx]
-        return self.nodes[node_id(nearest_coord[0], nearest_coord[1])]
+        for node in self.nodes.values():
+            if (node.x, node.y) == nearest_coord:
+                return node
     
     def reconstruct_path(self, p:Dict, start:Node, end:Node)-> List[Node]:
         path_nodes = []
@@ -175,7 +172,6 @@ class Graph:
                     cost_matrix[i,j] =calculate_route_cost(route)
         
         return cost_matrix, routes_matrix, mat_ids
-
 
     def dijkstra(self, startpoint:Tuple[float, float], endpoint:Tuple[float, float]) -> List[Node]:
         startpoint = self.find_nearest_node(startpoint)
@@ -230,12 +226,34 @@ def draw_graph(G: Graph):
         y_coords = [edge.start_node.y, edge.end_node.y]
         ax.plot(x_coords, y_coords, color='blue', linewidth=0.5)
 
-    #for node in G.nodes.values(): 
-    #    ax.scatter(node.x, node.y, color='k', s=5)
+    for node in G.nodes.values():
+        ax.scatter(node.x, node.y, color='k', s=5)
 
     ax.set_title("Wizualizacja grafu")
     ax.set_xlabel("Easting")
     ax.set_ylabel("Northing")
+
+class NodeManager:
+    def __init__(self):
+        self.node_indexes = {}
+        self.new_node_id = 0
+        self.tolerance = 1 # miejsca po przecinku, ujemne też działają
+
+    def manage_new_node(self, x: float, y: float) -> Node:
+        if f'({round(x, self.tolerance)}, {round(y, self.tolerance)})' in self.node_indexes:
+            return self.node_indexes[f'({round(x, self.tolerance)}, {round(y, self.tolerance)})']
+        node = Node(x, y)
+        node.id = self.new_node_id
+        self.new_node_id += 1
+        keyUU = f'({ceil(x, self.tolerance)}, {ceil(y, self.tolerance)})'
+        keyUD = f'({ceil(x, self.tolerance)}, {floor(y, self.tolerance)})'
+        keyDU = f'({floor(x, self.tolerance)}, {ceil(y, self.tolerance)})'
+        keyDD = f'({floor(x, self.tolerance)}, {floor(y, self.tolerance)})'
+        self.node_indexes[keyUU] = node
+        self.node_indexes[keyUD] = node
+        self.node_indexes[keyDU] = node
+        self.node_indexes[keyDD] = node
+        return node
 
 def build_graph_from_shapefile(file_path: str | List[str]) -> Graph:
     if isinstance(file_path, list):
@@ -245,6 +263,7 @@ def build_graph_from_shapefile(file_path: str | List[str]) -> Graph:
     else:
         gdf = gpd.read_file(file_path)
     G = Graph()
+    NM = NodeManager()
     G._shapefile_path = file_path
 
     # Iteracja po obiektach wczytanych do GeoDataFrame
@@ -254,7 +273,7 @@ def build_graph_from_shapefile(file_path: str | List[str]) -> Graph:
             feature = next(iterator)
         except StopIteration:
             break
-        G.add_feature(feature)
+        G.add_feature(feature, NM)
     G.build_kdtree()
     return G
 
@@ -328,33 +347,37 @@ def rand_route_points(points: List[Tuple[float, float]], route_mat:np.ndarray,
 
 # Demo
 if __name__ == "__main__":
-    shp_file_paths = ["kujawsko_pomorskie_m_Torun/L4_1_BDOT10k__OT_SKJZ_L.shp",
-                      "kujawsko_pomorskie_pow_torunski/L4_2_BDOT10k__OT_SKJZ_L.shp"]
+    shp_file_paths = ["..\\kujawsko_pomorskie_m_Torun/L4_1_BDOT10k__OT_SKJZ_L.shp",
+                      "..\\kujawsko_pomorskie_pow_torunski/L4_2_BDOT10k__OT_SKJZ_L.shp"]
     #test_path = r"C:\aszkola\5 sem\pag\projekt1\dane\testowe.shp"
-    test_path = r"C:\aszkola\5 sem\pag\projekt1\DROGIWYB\drogiwyb.shp"
+    test_path = r"..\test_shp.shp"
+    rozjechane = r"C:\Users\burb2\Desktop\Pliki Studia\PAG\rozjechane_drogi.shp"
     points_path = r"C:\aszkola\5 sem\pag\projekt1\PUNKTY.txt"
     points2_path =r"C:\aszkola\5 sem\pag\projekt1\Nawigacja-PAG\punkty2.txt"
 
-    graph = build_graph_from_shapefile(test_path)
+    graph = build_graph_from_shapefile(rozjechane)
     print(f"Graph has {len(graph.nodes)} nodes and {len(graph.edges)} edges.")
-
-    points = read_points(points_path)
-    cost_matrix, routes_matrix, mat_ids = graph.matrixes(points)
-    print(cost_matrix) 
-    points2 = read_points(points2_path)
-    full_route, total_cost, ids = rand_route_points(points2, routes_matrix, cost_matrix, mat_ids)
-
-    print(total_cost)
+    #
+    # points = read_points(points_path)
+    # cost_matrix, routes_matrix, mat_ids = graph.matrixes(points)
+    # print(cost_matrix)
+    # points2 = read_points(points2_path)
+    # full_route, total_cost, ids = rand_route_points(points2, routes_matrix, cost_matrix, mat_ids)
+    #
+    # print(total_cost)
     
     
 
-    #nodes = list(graph.nodes.values())
-    #route = graph.dijkstra(nodes[0], nodes[5])    
-
-    #draw_path(graph, route)
+    # nodes = list(graph.nodes.values())
+    # route = graph.dijkstra(nodes[0], nodes[5])
+    #
+    # draw_path(graph, route)
 
     #print(f"Node 3 {graph.nodes} nodes and {len(graph.edges)} edges."))
 
-    #draw_graph(graph)
-
-    #plt.show()
+    draw_graph(graph)
+    my_point = (473600, 571800)
+    nearest = graph.find_nearest_node(my_point)
+    draw_point(my_point)
+    draw_point((nearest.x, nearest.y))
+    plt.show()
